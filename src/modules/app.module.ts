322da47c8187841f09a4c1a33f13bd7411baea6e
dongha <donghaa90@gmail.com>
2023-02-01 17:53:25 +0900

    typeorm config add

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,14 +1,24 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { CommonModule } from './common';
-import { PassengerModule } from './passenger/passenger.module';
+import { CommonModule } from "./common";
+import { PassengerModule } from "./passenger/passenger.module";
 
 @Module({
     imports: [
         CommonModule,
-        TypeOrmModule.forRoot(),
-        PassengerModule
-    ]
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            host: process.env.TYPEORM_HOST,
+            port: 3306,
+            username: process.env.TYPEORM_USERNAME,
+            password: process.env.TYPEORM_PASSWORD,
+            database: process.env.TYPEORM_DATABASE,
+            synchronize: true,
+            entities: [__dirname + "/**/**/*.entity.ts"],
+            timezone: "Asia/Seoul",
+        }),
+        PassengerModule,
+    ],
 })
 export class ApplicationModule {}
